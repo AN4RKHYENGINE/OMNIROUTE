d5e1ea5bf6b7b27b5eafe5db80fe19631d7defe9
AN4RKHYENGINE <noreply@users.noreply.github.com>
2026-08-29 12:27:38 -0400

    fix: exclude native binaries from webpack bundling

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   distDir: ".build/next",
   output: "standalone",
+  serverComponentsExternalPackages: ["@ngrok/ngrok", "keytar", "koffi", "tls-client-node"],
+  webpack: (config, { isServer }) => {
+    config.externals.push("@ngrok/ngrok", "keytar", "koffi", "tls-client-node");
+    return config;
+  },
 };
 
 export default nextConfig;
